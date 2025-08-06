a169d7b831906da1f811418c5e4de834c2edcb08
Tsering Wolfe <tseringwolfe03@gmail.com>
2025-08-06 16:14:14 -0700

    writing data from identity screen into supabase table somewhat successsfully

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -26,12 +26,7 @@ export default function ProfileScreen() {
   const userSign = findAstrologySign();
 
   //ADDED state var for profile picture
-  const [profilePicUrl, setProfilePicUrl] = useState(
-<<<<<<< HEAD
-    "https://media.discordapp.net/attachments/979916344868872245/1401990490106101852/image.png?ex=689248e4&is=6890f764&hm=4002507880973412d011687c0057f983857e9cd1e565ab928cca7c687e2056ae&=&format=webp&quality=lossless&width=1408&height=1408",
-=======
-    "file:///Users/twolfe/Desktop/danny%20bitmojis/dannyCoffee.png",
->>>>>>> a68ecc4 (added button groups and state vars to identity screen)
+  const [profilePicUrl, setProfilePicUrl] = useState("file:///Users/twolfe/Desktop/danny%20bitmojis/dannyCoffee.png"
   );
 
   useEffect(() => {
@@ -94,10 +89,7 @@ export default function ProfileScreen() {
           title="Join School+"
         />
       </Pressable>
-<<<<<<< HEAD
       <Button onPress={handleSignOut} title="Log Out" />
-=======
->>>>>>> a68ecc4 (added button groups and state vars to identity screen)
       <Pressable>
         <Button
           onPress={() => {
@@ -106,7 +98,6 @@ export default function ProfileScreen() {
           title="Settings"
         />
       </Pressable>
-      <Button onPress={handleSignOut} title="Log Out" />
     </View>
   );
 }
