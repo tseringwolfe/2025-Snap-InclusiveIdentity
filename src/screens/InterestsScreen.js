13c27c71bd166eb811ffee7cb836580c24aa2985
Christine Pham <96cpham@gmail.com>
2025-08-10 02:29:19 -0700

    added functionality to the interest screen. able to have multiple categories with pills in all of them. able to select multiple

--- src/screens/InterestsScreen.js
+++ src/screens/InterestsScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { MultiSelect } from "react-native-element-dropdown";
 import {
     View,
     Text,
