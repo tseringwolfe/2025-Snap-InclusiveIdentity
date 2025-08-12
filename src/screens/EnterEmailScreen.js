8372f4d458534d859387839a75d250a94a67c20a
Christine Pham <96cpham@gmail.com>
2025-08-12 14:42:28 -0700

    Fixed having the school name show up properly onto EnterEmailScreen

--- src/screens/EnterEmailScreen.js
+++ src/screens/EnterEmailScreen.js
@@ -33,7 +33,6 @@ export default function EnterEmailScreen({ }) {
         }
         setError('');
         navigation.replace("Identity", {});
-        navigation.replace("Identity", {});
     };
 
 
